0f0a1519d411f831a1a9c9bbee65b2c82833b005
xiaoming <lininruc@gmail.com>
2024-05-16 08:39:17 +0800

    add mvs

--- check_app.py
+++ check_app.py
@@ -1,6 +1,7 @@
 #coding:utf-8
 from openai import OpenAI
 from dotenv import load_dotenv
+import json
 
 load_dotenv()
 
@@ -22,9 +23,16 @@ print(response.status)
 # 使用Batch ID来检索批处理状态
 batch_status = client.batches.retrieve("batch_gUwDs8Sdhw6DBEBp6JyPPXZa")
 
-# 检查批处理是否完成
+# # 检查批处理是否完成
 if batch_status.status == 'completed':
     # 使用output_file_id来下载结果
     output = client.files.content(batch_status.output_file_id)
     with open('output.json|', 'w') as file:
       file.write(str(output.content.decode('utf-8')))
+
+# 读取JSON Lines文件
+with open('output.json|', 'r') as file:
+    for line in file:
+        data = json.loads(line)  # 解析每一行的JSON数据
+        # 处理每个JSON对象
+        print(data['response']['body']['choices'][0]['message']['content'])
